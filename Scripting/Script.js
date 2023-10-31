2e63757317a0fe14d1bc59d20e4a51428e64195a
Theorignajjjaaa <59120862+Theorignajjjaaa@users.noreply.github.com>
2023-10-31 23:12:09 +0530

    vr 1.1 Test

---
+++ Scripting/Script.js
@@ -0,0 +1,14 @@
+let menuD = document.querySelector('.menudown')
+
+
+
+
+menuD.onclick = ()  =>{
+    if (menuD.innerHTML == "Menu UP") {
+        menuD.innerHTML = "Menu DN";
+      } 
+    else {
+        menuD.innerHTML = "Menu DN";
+    }
+    
+}
