6257e9e47cf124b9ba8c30c5102bfe463d0bec6a
Theorignajjjaaa <59120862+Theorignajjjaaa@users.noreply.github.com>
2023-11-17 17:41:25 +0530

    dARKmODE

--- Scripting/Script.js
+++ Scripting/Script.js
@@ -2,7 +2,8 @@ var i = 0;
 var txt = '..';
 var speed = 250;
 
-
+let Darktog = document.querySelector('.mod1');
+let Roots = document.querySelector(':root')
 
 
 
@@ -29,6 +30,18 @@ typeWriter()
 
 function onT() {
   window.open("https://www.youtube.com/watch?v=hvL1339luv0", "_blank");
+}
+
+function onDark() {
+  Darktog.classList.toggle('DarkMode');
+  Roots.classList.toggle('DarkMode');
+  if (Darktog.innerHTML == "L"){
+    Darktog.innerHTML = "D";
+  }
+  else{
+    Darktog.innerHTML = "L";
+  }
+  
 }
 
 
